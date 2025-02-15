f46eb022ca8ec132580b36c9bb1cf98cefe9d598
cjrem <remillardc15@gmail.com>
2025-02-15 13:09:27 -0500

    feat!: add game loop

--- src/java/Main.java
+++ src/java/Main.java
@@ -9,9 +9,38 @@ public class Main {
     // DRIVER //
     //
     public static void main(String[] args) {
+        init();
+        loop();
+    }
+
+
+    // GAME //
+    //
+    // game init
+    static void init() {
         INP.win(WIN);
         WIN.size(800, 600);
         WIN.build();
+    }
+    //
+    // game loop
+    static void loop() {
+        while (true) {
+
+            brake();
+        }
+    }
+
+
+    // SLEEP //
+    //
+    static void brake() {
+        try {
+            Thread.sleep(1);
+            // System.out.println("sleeping");
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
     }
 
 }
