a7081379bebfcac6bd39d2d2733d0ab641aba01a
cjRem44x <remillardc15@gmail.com>
2025-07-25 13:53:08 -0400

    fix: build batch path

--- build.bat
+++ build.bat
@@ -5,9 +5,9 @@ set proj=%cd%
 set src=%cd%\src\java
 set bin=%cd%\bin\java
 
-cd %src%
-javac -d %bin% *.java 
-java -cp %bin% Main
-cd %proj%
+cd "%src%"
+javac -d "%bin%" *.java 
+java -cp "%bin%" Main
+cd "%proj%"
 
 pause
